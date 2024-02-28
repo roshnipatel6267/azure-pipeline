307792d20de94eb292789945cc2dce22cdf8582f
roshni patel <engr.roshnipatel@gmail.com>
2024-02-28 18:24:54 +0530

    added

--- modules/nsg/variable.tf
+++ modules/nsg/variable.tf
@@ -18,28 +18,36 @@ variable "resource_group_name" {
 
 variable "nsg_rules" {
   type = list(object({
-    nsg_rules = [
-  {
-    name                       = "SSH"
-    priority                   = 100
-    access                     = "Allow"
-    protocol                   = "TCP"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  },
-  {
-    name                       = "HTTP"
-    priority                   = 101
-    access                     = "Allow"
-    protocol                   = "TCP"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-]
+    name                       = string
+    priority                   = number
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
   }))
   description = "List of rules for the Network Security Group"
+  default = [
+    {
+      name                       = "SSH"
+      priority                   = 100
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "HTTP"
+      priority                   = 101
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
 }
