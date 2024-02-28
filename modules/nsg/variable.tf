986407712edf2609b9cfdcaeb357cf76c198c984
roshni patel <engr.roshnipatel@gmail.com>
2024-02-28 18:18:11 +0530

    added

--- modules/nsg/variable.tf
+++ modules/nsg/variable.tf
@@ -18,14 +18,28 @@ variable "resource_group_name" {
 
 variable "nsg_rules" {
   type = list(object({
-    name                       = string
-    priority                   = number
-    access                     = string
-    protocol                   = string
-    source_port_range          = string
-    destination_port_range     = string
-    source_address_prefix      = string
-    destination_address_prefix = string
+    nsg_rules = [
+  {
+    name                       = "SSH"
+    priority                   = 100
+    access                     = "Allow"
+    protocol                   = "TCP"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  },
+  {
+    name                       = "HTTP"
+    priority                   = 101
+    access                     = "Allow"
+    protocol                   = "TCP"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+]
   }))
   description = "List of rules for the Network Security Group"
 }
